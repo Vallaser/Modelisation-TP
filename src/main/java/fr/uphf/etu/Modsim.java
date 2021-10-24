5d2ce0da69ff7f87ced5ff287d03780b81d1078c
Antoine Balieu <vhayn@outlook.com>
2021-10-24 13:31:40 +0200

    Create Gradle project

---
+++ src/main/java/fr/uphf/etu/Modsim.java
@@ -0,0 +1,13 @@
+package fr.uphf.etu;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Modsim extends Application {
+    public static void main(String[] args) {
+        Application.launch();
+    }
+
+    public void start(Stage stage) {
+    }
+}
